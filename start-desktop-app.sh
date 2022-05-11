68ee46619143a9cb863e6fb4bc013c7d97795e01
Vadim Merzhev <mergevvadim2202@gmail.com>
2022-05-11 23:34:48 +0300

    added start scripts and links to Quanterall profiles

---
+++ start-desktop-app.sh
@@ -0,0 +1,5 @@
+echo "Downgrading Node version to 16"
+nvm use 16
+
+echo "Starting Desktop application"
+npm run electron:serve
---
+++ start-web-app.sh
@@ -0,0 +1,5 @@
+echo "Downgrading Node version to 16"
+nvm use 16
+
+echo "Starting Web application"
+npm run serve
